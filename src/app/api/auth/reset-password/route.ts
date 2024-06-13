fd093bd7b30939b9d332d7c8807524b9787839b5
Gustavo Silva De Lima <gustavo.lima@cea.com.br>
2024-06-13 16:45:18 -0300

    testando reset

--- src/app/api/auth/reset-password/route.ts
+++ src/app/api/auth/reset-password/route.ts
@@ -17,18 +17,18 @@ export async function POST(req: Request) {
   user.code = undefined;
   user.codeExpiration = undefined;
 
-  if (!user) {
-    return new NextResponse("A senha não pode ser igual a anterior!", {
-      status: 400,
-    });
-  }
+//   if (!user) {
+//     return new NextResponse("A senha não pode ser igual a anterior!", {
+//       status: 400,
+//     });
+//   }
 
   try {
     return new NextResponse("Senha redefinida com sucesso", { status: 200 });
   } catch (error) {
     return new NextResponse(
       error instanceof Error ? error.message : "Unknown error",
-      { status: 500 }
+      { status: 400 }
     );
   }
 }
